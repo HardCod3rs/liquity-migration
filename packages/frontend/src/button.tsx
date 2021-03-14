286ecf0f6d98e96cb3d9aaceca61c360b9531365
FireBlackHat <firebot1020@gmail.com>
2021-03-14 14:01:23 +0330

    Frontend Init

---
+++ packages/frontend/src/button.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-alert */
+import React from "react";
+
+export const Button = () => (
+  <button type="button" onClick={() => alert(`the meaning if life is ${10}`)}>
+    Click me
+  </button>
+);
